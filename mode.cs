4ca1df140d5db9c6133cb5a127bcb20d93c5b89a
argsv <alsaydi23@yahoo.com>
2014-07-25 15:03:49 -0500

    Update mode.cs
    this probably works; need more test cases

--- mode.cs
+++ mode.cs
@@ -33,7 +33,9 @@ namespace ConsTest
             list.Add(new int[] { 2, 3, 2, 1, 3, 3 });
             list.Add(new int[] { 2, 3, 2, 3, 3, 3 });
             list.Add(new int[] { 2, 2, 2, 3, 3, 3 });
-            list.Add(new int[] { 1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2 });
+            list.Add(new int[] { 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1 });
+            list.Add(new int[] { 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1 });
+            list.Add(new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2 });
             list.Add(new int[] { 1, 1, 2, 1, 1, 2, 1, 1, 2, 3, 3, 3, 3, 3, 3, 1 });
             list.Add(new int[] { 1, 1, 1, 4, 1, 1, 4, 1, 4, 1, 1, 1, 4, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3 });
             list.Add(new int[] { 2, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 4, 1, 1, 4, 1, 4, 1, 1, 1, 4, 1, 1, 1 });
@@ -41,7 +43,7 @@ namespace ConsTest
             list.Add(new int[] { 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 1, 5, 1, 1, 1, 5, 1, 1, 1, 5, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 });
             list.Add(new int[] { 1, 3, 3, 3, 3, 3, 3, 2, 1, 1, 2, 1, 1, 2, 1, 1 });
             list.Add(new int[] { 4, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 5, 1, 1, 1, 7, 7, 7, 1, 1, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7, 1, 7 });
-            Console.WriteLine("Last List Count: {0}",list.Last().Count());
+            /*Console.WriteLine("Last List Count: {0}",list.Last().Count());
             Console.WriteLine("Mode: {0}", list.Last().Where(a => a == 1).Count());
             Console.WriteLine("Runner up: {0}", list.Last().Where(a => a == 2).Count());
             var last = list.Last();
@@ -51,7 +53,7 @@ namespace ConsTest
             Console.WriteLine("MidPoint: {0}", last[last.Length / 2]);
             Console.WriteLine("MidPoint-1: {0}", last[last.Length / 2-1]);
             Console.WriteLine("MidPoint+1: {0}", last[last.Length / 2+1]);
-            
+            */
             //FindModeLinear(last, true);
             foreach (var arr in list)
             {
@@ -81,19 +83,20 @@ namespace ConsTest
                     }
                 }
             }
-            //TestUsingRandomArrays();
+            //TestUsingRandomArrays();            
         }
 
         private static void TestUsingRandomArrays()
         {
-            int N = 1000;
-            int reps = 1000;
+            int N = 100;
+            int reps = 1000000;
             for (int i = 0; i < reps; i++)
             {
                 var arr = BuildArray(N);
                 //PrintArray(arr);
                 int foundMode = FindMode(arr, 0, arr.Length - 1).Mode;
                 int lMode = FindModeLinear(arr, false);
+                //Console.WriteLine("Mode found: {0}", lMode);
                 if (foundMode != lMode)
                 {
                     if (!(arr.Distinct().Count() == 2))
@@ -105,10 +108,6 @@ namespace ConsTest
                         Console.WriteLine(arr.Length);
                         break;
                     }
-                }
-                else
-                {
-                  //  Console.WriteLine("Mode found: {0}", lMode);
                 }
             }
         }
@@ -293,7 +292,7 @@ namespace ConsTest
             int runnerUp = 0;
             int runnerUpReps = 0;
             mostRepeated = arr[0];
-            mostRepeatedReps = 1;            
+            mostRepeatedReps = 1;
             int others = 0;
             int len = arr.Length;
             if (debug)
@@ -306,17 +305,15 @@ namespace ConsTest
                 {
                     Console.Write("{0} ", arr[i]);
                 }
-                if ((i+1)*2>len)
-                {
-                    if (others > mostRepeatedReps)/* we passed the mid point of the array but the most repeated element so far is repeated fewer times than 
+                if (others > mostRepeatedReps)/* we passed the mid point of the array but the most repeated element so far is repeated fewer times than 
                                                    * the other elements (exclude 2nd most repeated and 3rd most repeated); we need to reset counters, mode will be in the remaining half of the array. */
-                    {
-                        mostRepeated = arr[i-1];
-                        mostRepeatedReps = 1;
-                        runnerUpReps = 0;                        
-                        others = 0;                        
-                    }
+                {
+                    mostRepeated = arr[i - 1];
+                    mostRepeatedReps = 1;
+                    runnerUpReps = 0;
+                    others = 0;
                 }
+                
                 if (arr[i] == mostRepeated)
                 {
                     mostRepeatedReps++;
@@ -396,9 +393,59 @@ namespace ConsTest
                     }
                 }
             }
+            if (debug)
+            {
+                Console.WriteLine();
+            }
             if (runnerUpReps > mostRepeatedReps)
             {
                 mostRepeated = runnerUp;
+            }           
+            //Console.WriteLine(mostRepeatedReps);
+            return mostRepeated;
+        }
+        /// <summary>
+        /// simpler but does not work
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="debug"></param>
+        /// <returns></returns>
+        static int _FindModeLinear(int[] arr, bool debug)
+        {
+            int mostRepeated = 0;
+            int mostRepeatedReps = 0;
+            mostRepeated = arr[0];
+            mostRepeatedReps = 1;
+            int others = 0;
+            int len = arr.Length;
+            if (debug)
+            {
+                Console.Write("{0} ", arr[0]);
+            }
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (debug)
+                {
+                    Console.Write("{0} ", arr[i]);
+                }
+                if (arr[i] == mostRepeated)
+                {
+                    mostRepeatedReps++;
+                }
+                else
+                {
+                    if (others > mostRepeatedReps)/* we passed the mid point of the array but the most repeated element so far is repeated fewer times than 
+                                                   * the other elements (exclude 2nd most repeated and 3rd most repeated); we need to reset counters, mode will be in the remaining half of the array. */
+                    {
+                        others = 0;                        
+                        mostRepeated = arr[i];
+                        mostRepeatedReps = 1;
+                    }
+                    else
+                    {
+                        others++;
+                    }
+                }
             }
             if (debug)
             {
